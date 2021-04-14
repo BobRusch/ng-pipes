47a20cb0f72b0cf846ffc591ada5136d5c7217e0
Bob Rusch <git-commit-address>
2021-04-14 08:36:07 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: string = '';
+  height: string = '';
+  miles: string = '';
+
+  onNameChanged(value: string) {
+    this.name = value;
+  }
+
+  onDateChanged(value: string) {
+    this.date = value;
+  }
+
+  onAmountChanged(value: string) {
+    this.amount = value;
+  }
+
+  onHeightChanged(value: string) {
+    this.height = value;
+  }
+
+  onMilesChanged(value: string) {
+    this.miles = value;
+  }
 }
